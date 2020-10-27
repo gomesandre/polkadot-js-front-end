915a1a4de60e46cbe63ebbd43d786003e09f5931
André Gomes <andregms@outlook.com>
2020-10-27 01:37:31 -0300

    Hello World! by Polkadot - Front-End Javascript

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+
+class App extends React.Component {
+  
+  state = {
+    block: {
+      number: 0,
+      hash: "0x",
+    }
+  }
+
+  componentDidMount() {
+    this.getLatestBlock();
+  }
+
+  getLatestBlock = async () => {
+    const api = await ApiPromise.create({ provider: wsProvider });
+    
+    let count = 0;
+    
+    const unsubscribe = await api.rpc.chain.subscribeNewHeads((header) => {
+      this.setState({ block: { number: header.number.toNumber(), hash: header.hash.toHex() }});
+
+      if (++count === 256) {
+        unsubscribe();
+        process.exit(0);
+      }
+    });
+  };
+
+  render () {
+    let {number, hash} = this.state.block;
+
+    if (number !== 0) {
+      return <div>
+        <div>Block number: { number }</div>
+        <div>Block hash: { hash }</div>
+      </div>;  
+    }
+
+    return <div>
+      <div>Loading Polkadot latest block information...</div>
+    </div>;
+  }
 }
 
 export default App;
